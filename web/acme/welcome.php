0de843b285e4b9a08e44cf5c806234e49e8c115c
Paz01 <60375767+Paz01@users.noreply.github.com>
2020-05-23 18:55:30 -0400

    Acme updates

--- web/acme/welcome.php
+++ web/acme/welcome.php
@@ -27,9 +27,9 @@
         //<button class="btn btn-success mt-3" name="Logout">Logout</button>
         //<button class="btn btn-success mt-3" name="Login">Login</button>
 
-        echo '<a href="acme.php?Control Panel" <button class="btn btn-success mt-3 btn-block" name="Control Panel">Control Panel</button></a>';
+        echo '<a href="acme.php?Control Panel" <button class="btn btn-primary mt-3 btn-block" name="Control Panel">Control Panel</button></a>';
         
-        echo '<a href="logout.php?logout" <button class="btn btn-danger mt-3 btn-block" name="Logout">Logout</button></a>';
+        echo '<a href="logout.php?logout" <button class="btn btn-warning mt-3 btn-block" name="Logout">Logout</button></a>';
     }
     else
     {
