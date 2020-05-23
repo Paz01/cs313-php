da387fca25ce8c6e8d249274b8cf7aeeac91ab90
Paz01 <60375767+Paz01@users.noreply.github.com>
2020-05-23 17:01:52 -0400

    Acme updates

--- web/acme/welcome.php
+++ web/acme/welcome.php
@@ -1,5 +1,23 @@
-<?php
-    session_start();
+session_start();
+
+<!DOCTYPE html>
+
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Welcome Page</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="">
+    </head>
+
+    <link href="css/bootstrap.css" rel="stylesheet">
+
+    <body>
+      
+    <?php
+    
  
     if(isset($_SESSION['User']))
     {
@@ -29,3 +47,21 @@
     }
  
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        
+    </body>
+</html>
