653e0f20aa7e080aeed34fc59e1077154f4d4f8e
Paz01 <60375767+Paz01@users.noreply.github.com>
2020-05-30 19:39:40 -0400

    Acme updates

--- web/acme/acme.php
+++ web/acme/acme.php
@@ -25,7 +25,7 @@ $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
       <br>
         <div class="container">
             <h3 class="text-center py-3">Control Panel - Acme Enterprises</h3>
-            <a href="customer.php?customer" <button class="btn btn-primary mt-3 " name="Logout">Create customer</button> </a>
+            <a href="customer.php" <button class="btn btn-primary mt-3 " name="Logout">Create customer</button> </a>
                 <br>
                     <table class ="table table-hover">
                     
