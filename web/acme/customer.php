1483863ffbf01cd80df2365a2f7760a52a5be475
Paz01 <60375767+Paz01@users.noreply.github.com>
2020-05-30 18:17:41 -0400

    Acme updates

--- web/acme/customer.php
+++ web/acme/customer.php
@@ -1,4 +1,3 @@
-
 <?php 
 require_once('dbConnect.php');
 $db = get_db();
@@ -18,7 +17,6 @@ if(!empty($_POST))
     $stmt = $db->prepare($query);
     $stmt->execute();
     $stmt->fetchALL(PDO::FETCH_ASSOC); 
-       
 }
 // Update a Record
 if(!empty($_GET))
@@ -29,12 +27,9 @@ if(!empty($_GET))
     $stmt = $db->prepare($query);
     $stmt->execute();
     $info = $stmt->fetchALL(PDO::FETCH_ASSOC); 
-   
 }
-
 // INSERT INTO customer (first_Name, last_Name, phone, email) 
 // VALUES ('John', 'Smith', '817-845-4574', 'Smith@gmail.com');
-
 ?>
 
 <!DOCTYPE html>
@@ -50,16 +45,18 @@ if(!empty($_GET))
 
     <body>
         <div class="container">
+            <h2 class="text-center py-3">Customer information</23>
                 <table class ="table">
                     
                     <form action="customer.php" method="POST">
 
-                    <div class="form-group">
-                      <label for="First Name">First Name:</label>
-                      <input type="text" class="form-control" id="uname" placeholder="Enter First Name" name="first" required>
-                      <div class="valid-feedback">Valid.</div>
-                      <div class="invalid-feedback">Please fill out this field.</div>
-                    </div>
+                        <div class="form-group">
+                        <label for="First Name">First Name:</label>
+                        <input type="text" class="form-control" id="uname" input value = "<?php echo $info [0]->first_name?>"
+                                placeholder="Enter First Name" name="first" required>
+                        <div class="valid-feedback">Valid.</div>
+                        <div class="invalid-feedback">Please fill out this field.</div>
+                        </div>
 
                     
                     <!--<input value = "<?php //echo $info [0]->first_name?>" type ="text" name ="first" placeholder = "First Name">-->
