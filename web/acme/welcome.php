a73cf0ed0b894b43fd1552dac0fcf7fe015df22b
Paz01 <60375767+Paz01@users.noreply.github.com>
2020-06-05 19:09:41 -0400

    Acme updates

--- web/acme/welcome.php
+++ web/acme/welcome.php
@@ -8,9 +8,7 @@
     {
         echo ' Welcome ' . $_SESSION['User']. ':'. '<br/>';
         echo '<br/>';
-        
-        
-        
+                       
         echo 'We are thrilled that you are part of Acme Enterprises. ';
         echo 'This is your main landing page where you will be updating a series of activities, your primary role ' ; 
         echo 'is to update the activity of customer orders, their information and track their orders ';
@@ -21,26 +19,13 @@
         echo 'Click on the links below to continue';
 
         echo '<br/>';
+             
+
+        echo '<a href="job.php?Job Control Panel" <button class="btn btn-success mt-3 " name="Job Control Panel">Job Control Panel</button></a>';
+
+        echo '<a href="acme.php?Control Panel" <button class="btn btn-success mt-3 " name="Customer Control Panel">Customer Control Panel</button></a>';
         
-        //echo '<a href = "acme.php">Acme Control Panel</a><br/>';
-
-        //echo '<br/>';
-        //echo "Otherw"
-
-        //<button class="btn btn-success mt-3" name="Login">Login</button>
-
-        //echo '<button class="btn btn-success mt-3" name="Login">Login</button>
-
-        // echo '<a href="logout.php?logout">Logout</a>';
-
-        //echo '<a href="logout.php" <button class="btn btn-success mt-3" name="Logout">Logout</button> </a>';
-              
-        //<button class="btn btn-success mt-3" name="Logout">Logout</button>
-        //<button class="btn btn-success mt-3" name="Login">Login</button>
-
-        echo '<a href="acme.php?Control Panel" <button class="btn btn-success mt-3 btn-block" name="Control Panel">Control Panel</button></a>';
-        
-        echo '<a href="logout.php?logout" <button class="btn btn-primary mt-3 btn-block" name="Logout">Logout</button></a>';
+        echo '<a href="logout.php?logout" <button class="btn btn-primary mt-3 " name="Logout">Logout</button></a>';
     }
     else
     {
