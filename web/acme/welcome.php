1b4e4f38aca8891ab554984c831bcd7ffb8a4de3
Paz01 <60375767+Paz01@users.noreply.github.com>
2020-06-07 01:10:31 -0400

    Acme updates

--- web/acme/welcome.php
+++ web/acme/welcome.php
@@ -11,7 +11,7 @@
                        
         echo 'We are thrilled that you are part of Acme Enterprises. ';
         echo 'This is your main landing page where you will be updating a series of activities, your primary role ' ; 
-        echo 'is to update the activity of customer orders, their information and track their orders ';
+        echo 'is to update customer information and track their orders ';
         echo 'If you have any questions please contact your manager in charge. ';
 
         echo '<br/>';
@@ -21,10 +21,12 @@
         echo '<br/>';
              
 
-        echo '<a href="job.php?Job Control Panel" <button class="btn btn-success mt-3 " name="Job Control Panel">Job Control Panel</button></a>';
-        echo '<br/>';
         echo '<a href="acme.php?Customer Control Panel" <button class="btn btn-success mt-3 " name="Customer Control Panel">Customer Control Panel</button></a>';
         echo '<br/>';
+        
+        echo '<a href="job.php?Job Control Panel" <button class="btn btn-success mt-3 " name="Job Control Panel">Job Control Panel</button></a>';
+        echo '<br/>';
+        
         echo '<a href="logout.php?logout" <button class="btn btn-primary mt-3 " name="Logout">Logout</button></a>';
     }
     else
