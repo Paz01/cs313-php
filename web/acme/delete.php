d44c664639eeb918a80bc3c55db35ee14ec8b7b4
Paz01 <60375767+Paz01@users.noreply.github.com>
2020-06-06 22:17:38 -0400

    Acme updates

--- web/acme/delete.php
+++ web/acme/delete.php
@@ -1,6 +1,5 @@
 
 <?php
-
 require_once('dbConnect.php');
 $db = get_db();
 
@@ -26,10 +25,26 @@ $db = get_db();
             echo "Can not delete record";
         }
     }
-
-    echo '<br/>';
-             
-        echo '<a href="job.php?Job Control Panel" <button class="btn btn-success mt-3 " name="Job Control Panel">Job Control Panel</button></a>';
-        echo '<br/>';
-        echo '<a href="acme.php?Customer Control Panel" <button class="btn btn-success mt-3 " name="Customer Control Panel">Customer Control Panel</button></a>';
+     
 ?>
+
+<!DOCTYPE html>
+<head>
+  <title>Job Orders</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"></script>
+  <!--<link href="css/bootstrap.css" rel="stylesheet"> -->
+
+
+  <body style="background:#CCC;">
+
+        <br/>        
+        <a href="job.php?Job Control Panel" <button class="btn btn-success mt-3 " name="Job Control Panel">Job Control Panel</button></a>
+        <br/>
+        <a href="acme.php?Customer Control Panel" <button class="btn btn-success mt-3 " name="Customer Control Panel">Customer Control Panel</button></a>
+        </body>
+</html>
