43a563cfd06a74c46444e8f80a5dfd7ced137a33
Paz01 <60375767+Paz01@users.noreply.github.com>
2020-06-08 23:54:34 -0400

    Acme updates

--- web/acme/order.php
+++ web/acme/order.php
@@ -16,7 +16,7 @@ if(!empty($_POST) && empty($_GET))
     $employee_name = (int)$_POST['employee_name'];
     
     //INSERT INTO job (notes, price, customer_id, service_id, employee_id) VALUES ('The job went smooth', '75.00',1,1,1);
-    //$query = "INSERT INTO job (notes, price, customer_id, service_id employee_id) VALUES ($notes, $price, $customer_name, $service_name, $employee_name);";
+    $query = "INSERT INTO job (notes, price, customer_id, service_id employee_id) VALUES ($notes, $price, $customer_name, $service_name, $employee_name);";
 
     //$stmt = $db->prepare($query);
     
